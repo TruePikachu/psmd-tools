38254bfa00afe6438a44b2f18094da40f65b6af3
Chris Dusto <cricket_lover@live.com>
2016-01-29 17:47:11 -0800

    Added a system for managing offsets into exefs/code.bin

---
+++ systems/bin-offsets.asd
@@ -0,0 +1,6 @@
+(defsystem "bin-offsets"
+  :description "PSMD Binary Offset Manager"
+  :version "1.0"
+  :author "Chris Dusto <cricket_lover@live.com>"
+  :depends-on ()
+  :components ((:file "bin-offsets")))
---
+++ systems/bin-offsets.lisp
@@ -0,0 +1,12 @@
+(defpackage :rom-offsets
+  (:nicknames :bo)
+  (:use :common-lisp)
+  (:export :offset))
+(in-package :rom-offsets)
+(defvar *offset-list*
+  (with-open-file (in "/psmd/data/offsets.dat")
+    (with-standard-io-syntax
+      (read in))))
+(defun offset (name)
+  "Get the offset that has the specified name keyword"
+  (getf *offset-list* name))
